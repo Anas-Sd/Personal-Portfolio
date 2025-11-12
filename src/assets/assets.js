0e56a00ce975881dbd65481b28b7c4999cd3c515
Anas-Sd <anasannu44455@gmail.com>
2025-11-12 16:29:37 +0530

    Updated urls as routes

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,43 +6,58 @@ import Automation_Anywhere_RPA from "@/assets/Automation_Anywhere_RPA.png";
 
 export const certifications = [
   {
-    name: "Microsoft AZ-900",
-    url: "/MICROSOFT AZURE AZ-900 CERTIFICATE.pdf",
+    bigname: "Azure AZ-900",
+    name: "Microsoft",
+    url: "/Azure",
     logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    issuedOn : "08/08/2025",
+    credentialID : "ozxr-uSez"
   },
   {
-    name: "Oracle Certified Foundations Associate",
-    url: "/Oracle Certified Foundations Associate Certificate.pdf",
+    bigname: "Foundations Associate",
+    name: "Oracle",
+    url: "/Oracle-Foundations-Associate",
     logo: "https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg",
+    issuedOn : "07/09/2025",
+    credentialID : "102560148OCI25FNDCFA"
   },
+//   {
+//     name: "Oracle Certified Architect Associate",
+//     url: "/Oracle Cloud Infrastructure 2025 Certified Architect Associate.pdf",
+//     logo: "https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg",
+//   },
   {
-    name: "Oracle Certified Architect Associate",
-    url: "/Oracle Cloud Infrastructure 2025 Certified Architect Associate.pdf",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg",
-  },
-  {
-    name: "Salesforce Certified AI & Cloud Associate",
-    url: "/Salesforce Certified AI Associate Certificate.pdf",
+    bigname: "Agentforce Specialist",
+    issuedOn : "13/09/2025",
+    credentialID : "6758078",
+    name: "Salesforce",
+    url: "/Salesforce-Agentforce-Specialist",
     logo: "https://upload.wikimedia.org/wikipedia/commons/f/f9/Salesforce.com_logo.svg",
   },
   {
-    name: "AWS AI/ML Virtual Internship (EduSkills)",
-    url: "/SYED ANAS AWS AIML VIRTUAL INTERNSHIP.pdf",
+     bigname: "AIML Virtual Internship",
+    issuedOn : "10/06/2025",
+    credentialID : "0de15c82e9068f86545983cc92b18511",
+    name: "By AWS - via EduSkills",
+    url: "/AIML-Virtual-Internship",
     logo: "https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg",
   },
+//   {
+//     name: "ACE Aviatrix Multi Cloud Network Associate",
+//     url: "/AVIATRIX ACE MULTICLOUD NETWORK ASSOCIATE CERTIFICATE.pdf",
+//     logo: aviatrixLogo,
+//   },
   {
-    name: "ACE Aviatrix Multi Cloud Network Associate",
-    url: "/AVIATRIX ACE MULTICLOUD NETWORK ASSOCIATE CERTIFICATE.pdf",
-    logo: aviatrixLogo,
-  },
-  {
-    name: "Cambridge Lingua Skills",
-    url: "/Lingua Skill Certification and Test Report.pdf",
+     bigname: "English - Lingua Skills",
+    issuedOn : "20/05/2024",
+    credentialID : "IA143KLV-SEP2025-211",
+    name: "Cambridge University",
+    url: "/Lingua-Skills",
     logo: cambridgeLogo,
   },
-  {
-    name: "RPA Certified Essentials Automation Professional",
-    url: "/RPA Certified Essentials Automation Professional Certificate.pdf",
-    logo: Automation_Anywhere_RPA,
-  },
+//   {
+//     name: "RPA Certified Essentials Automation Professional",
+//     url: "/RPA Certified Essentials Automation Professional Certificate.pdf",
+//     logo: Automation_Anywhere_RPA,
+//   },
 ];
