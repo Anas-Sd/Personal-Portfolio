b6e28d0d5d0b22c55285c6969ad05abf5efebbc8
Anas-Sd <anasannu44455@gmail.com>
2025-11-12 16:29:07 +0530

    Added routes for certificates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Resume from "@/components/Resume";
+import Resume from "@/Pdf's/Resume";
+import Azure_Certificate from "@/Pdf's/Azure_Certificate";
+import Oracle_FA_Certificate from "@/Pdf's/Oracle_FA_Certificate";
+// import Oracle_AA_Certificate from "@/Pdf's/Oracle_AA_Certificate";
+import Salesforce_Certificate from "@/Pdf's/Salesforce_Certificate";
+import Internship_Certificate from "@/Pdf's/Internship_Certificate";
+import Lingua_Skills_Certificate from "./Pdf's/Lingua_Skills_Certificate";
 
 const queryClient = new QueryClient();
 
@@ -18,7 +24,13 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="/resume" element={<Resume />} />
+          <Route path="/Resume" element={<Resume />} />
+          <Route path="/Azure" element={<Azure_Certificate />} />
+          <Route path="/Oracle-Foundations-Associate" element={<Oracle_FA_Certificate />} />
+          {/* <Route path="/Oracle-Architect-Associate" element={<Oracle_AA_Certificate />} /> */}
+          <Route path="/Salesforce-Agentforce-Specialist" element={<Salesforce_Certificate />} />
+          <Route path="/AIML-Virtual-Internship" element={<Internship_Certificate />} />
+          <Route path="/Lingua-Skills" element={<Lingua_Skills_Certificate />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
