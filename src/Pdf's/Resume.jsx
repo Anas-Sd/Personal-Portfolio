f07f89fd437efdf5140cb6d55fa23d0f7c8bed03
Anas-Sd <anasannu44455@gmail.com>
2025-11-26 01:11:03 +0530

    updated resume

--- src/Pdf's/Resume.jsx
+++ src/Pdf's/Resume.jsx
@@ -8,7 +8,7 @@ export default function Resume() {
   return (
     <div className="w-full h-screen bg-gray-100 flex flex-col items-center justify-center">
       <iframe
-        src="/SYED_ANAS_RESUME_complete-94.pdf"
+        src="/SYED_ANAS_RESUME_3.1.pdf"
         className="w-full h-full border-none"
         title="Syed Anas - Resume"
       ></iframe>
